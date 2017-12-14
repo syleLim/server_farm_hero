8e482d580e870e263b556cfaf3afe2efd4919ba1
syleLim <syle94@gmail.com>
2017-12-14 15:13:34 +0900

    sibal

--- routes/Data_Base/db.js
+++ routes/Data_Base/db.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 var client = mysql.createConnection({
 	//TODO : DB connect
-	host : 'us-cdbr-iron-east-05.cleardb.net'
+	host : 'us-cdbr-iron-east-05.cleardb.net',
 	user : 'bf2c3bbc87c230',
 	password : 'cceac6ea',
 	database : 'heroku_37d0896afc5e7ad'
