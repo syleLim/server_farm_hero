9800aba35750faf22c9df7f60eff3c6a6412c014
syleLim <syle94@gmail.com>
2017-12-14 15:11:50 +0900

    db up

--- routes/Data_Base/db.js
+++ routes/Data_Base/db.js
@@ -2,9 +2,10 @@ var mysql = require('mysql');
 
 var client = mysql.createConnection({
 	//TODO : DB connect
-	user : 'root',
-	password : 'sql8439kor!A',
-	database : 'farm'
+	host : 'us-cdbr-iron-east-05.cleardb.net'
+	user : 'bf2c3bbc87c230',
+	password : 'cceac6ea',
+	database : 'heroku_37d0896afc5e7ad'
 });
 
 module.exports  = client;
